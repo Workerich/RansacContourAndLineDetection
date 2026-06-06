8595433e78519c667bce937ac4a000b8cd4c1126
Workerich <highmat32@gmail.com>
2026-06-06 14:47:07 +0300

    ransac fixed

--- ransac.py
+++ ransac.py
@@ -218,12 +218,11 @@ def ransac_parabola(arr, iterations=100, E=5):
 
 
 # OTHER
-def find_contours(arr, ransac_model, n, w, E, min_length, max_lines, filtered=True):
+def find_contours(arr, ransac_model, iterations, w, E, min_length, max_lines, filtered=True):
     all_lines = []
     remaining = arr
-    k = int(math.log(1 - 0.99) / math.log(1 - w ** n))
-    print(k, "Итераций")
     anomalies = int(len(arr) * (1 - w) / max_lines)
+    k = iterations
     print(anomalies, "Теоретическое кол-во выбросов")
 
     for _ in range(max_lines):
@@ -253,11 +252,13 @@ def process_ransac(path, model_type, find_contour, w, E, min_length, max_contour
     img = cv2.imread(path)
     arr = get_points_from_image(img, threshold=100)
     ransac_model, min_num_points, model_name = ransac_models[model_type]
+    k = math.ceil(math.log(1 - 0.99) / math.log(1 - w ** min_num_points))
+    print(k, "Итераций")
 
     if find_contour:
         created = time.time()
-        good_contours = find_contours(arr, ransac_model, min_num_points,
-                                      w, E, min_length, max_contours, filtered)
+        good_contours = find_contours(arr, ransac_model, k, w, E, min_length,
+                                      max_contours, filtered)
         ended = time.time()
         good_points = set()
         for line in good_contours:
@@ -268,7 +269,7 @@ def process_ransac(path, model_type, find_contour, w, E, min_length, max_contour
 
     else:
         created = time.time()
-        good_points = ransac_model(arr, iterations=1000, E=5)
+        good_points = ransac_model(arr, k, E)
         ended = time.time()
         msg = (f"Модель - {model_name}\n"
                f"Время: {round(ended - created, 2)} cек")
